36e62c539e52f8806ecd4fdc2417f1e7748a8172
Warren Thompson <wathompson@epiqsystems.com>
2017-08-01 17:12:31 -0700

    In memory adding works

--- SayHiVM/SupportChatComponentVM.cs
+++ SayHiVM/SupportChatComponentVM.cs
@@ -5,24 +5,34 @@ using System.Threading;
 using DotNetify;
 namespace ViewModels
 {
+    public class SupportChatMessage
+    {
+        public int type;
+        public string image;
+        public string text;
+    }
+
     public interface ISupportMessageService
     {
-        List<SupportChatComponentVM.SupportChatMessage> GetAll();
+        List<SupportChatMessage> GetAll();
+
+        void Add(SupportChatMessage newMesssage);
     }
 
     public class ListSupportMessageService : ISupportMessageService
     {
-        List<SupportChatComponentVM.SupportChatMessage> ISupportMessageService.GetAll()
+
+        public ListSupportMessageService()
         {
-            return new List<SupportChatComponentVM.SupportChatMessage>()
+            _allChatMessages = new List<SupportChatMessage>()
             {
-                new SupportChatComponentVM.SupportChatMessage()
+                new SupportChatMessage()
                 {
                     image = "http://lorempixel.com/50/50/cats/",
                     text = "Hello! Good Morning!",
                     type = 0
                 },
-                new SupportChatComponentVM.SupportChatMessage()
+                new SupportChatMessage()
                 {
                     image = "http://lorempixel.com/50/50/animals/",
                     text = "Hello to you! Good Morning!",
@@ -30,20 +40,37 @@ namespace ViewModels
                 }
             };
         }
-    }
 
-    public class SupportChatComponentVM : BaseVM
-    {
-        public class SupportChatMessage
+        private List<SupportChatMessage> _allChatMessages;
+
+        public void Add(SupportChatMessage newMesssage)
         {
-            public int type;
-            public string image;
-            public string text;
+            _allChatMessages.Add(newMesssage);
         }
 
+
+        public List<SupportChatMessage> GetAll()
+        {
+            return _allChatMessages;
+        }
+    }
+
+    // ReSharper disable once InconsistentNaming
+    public class SupportChatComponentVM : BaseVM
+    {
+
         /// <summary>
-        /// List of employees.
+        /// When the Add button is clicked, this property will receive the new employee full name input.
         /// </summary>
+        public Action<SupportChatMessage> Add => chatMessage =>
+        {
+            _supportMessageService.Add(chatMessage);
+        };
+
+        /// <summary>
+        /// List of all messages.
+        /// </summary>
+        // ReSharper disable once InconsistentNaming
         public IEnumerable<SupportChatMessage> messages => _supportMessageService.GetAll().Select(i => new SupportChatMessage
         {
             type = i.type,
